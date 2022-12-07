625c373786a9c94a4e5ec0ca1d0c192a26fc0cb9
brightamoi <amoi.bright92@gmail.com>
2022-12-07 16:03:48 -0500

    about uodated

--- flow-app/pages/about.js
+++ flow-app/pages/about.js
@@ -24,16 +24,6 @@ export default function About() {
             changes.
           </li>
         </ul>
-
-        <p>
-          Use this app as a starting template to build your very own web3 app,
-          powered on Flow! Flow is a fast, decentralized, and developer-friendly
-          blockchain, designed as the foundation for a new generation of games,
-          apps, and the digital assets that power them. It is based on a unique,
-          multi-role architecture, and designed to scale without sharding,
-          allowing for massive improvements in speed and throughput while
-          preserving a developer-friendly, ACID-compliant environment.
-        </p>
       </div>
     </div>
   );
